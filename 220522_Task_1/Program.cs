ae16f257f923f701fbb7eeec6cf287b471876a4b
vmaevski <vmaevski@mail.ru>
2022-05-24 07:42:08 +0700

    Task 1 completed

---
+++ 220522_Task_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] RandomArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintEvenNumbers(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            Console.Write($"{array[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
+
+void PrintArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = RandomArray(8, 100, 999);
+PrintArray(array);
+PrintEvenNumbers(array);

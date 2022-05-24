f66696f694b61a0177b5d52fbaee1a1c9aefcdd1
vmaevski <vmaevski@mail.ru>
2022-05-24 19:56:39 +0700

    Task 2 completed

--- 220522_Task_2/Program.cs
+++ 220522_Task_2/Program.cs
@@ -1,14 +1,14 @@
 ﻿// Задайте одномерный массив, заполненный случайными числами. 
 // Найдите сумму элементов, стоящих на нечётных позициях.
 
-// *** Понял так, что нужно работать с элемнетами массива с нечётнымии ИНДЕКСАМИ (а не номерами по порядку).
+// *** Понял так, что нужно работать с элементами массива с нечётнымии ИНДЕКСАМИ (а не номерами по порядку).
 
 int[] RandomArray(int length, int minValue, int maxValue)
 {
     int[] array = new int[length];
     for (int i = 0; i < length; i++)
     {
-        array[i] = new Random().Next(minValue, maxValue +1);
+        array[i] = new Random().Next(minValue, maxValue + 1);
     }
     return array;
 }
@@ -26,12 +26,9 @@ void PrintArray(int[] array)
 void PrintOddIndices(int[] array)
 {
     int length = array.Length;
-    for (int i = 0; i < length; i++)
+    for (int i = 1; i < length; i += 2)
     {
-        if (i % 2 != 0)
-        {
-            Console.Write($"{array[i]} ");
-        }     
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
@@ -40,12 +37,9 @@ int SumOfOddIndices(int[] array)
 {
     int sum = 0;
     int length = array.Length;
-    for (int i = 0; i < length; i++)
+    for (int i = 1; i < length; i += 2)
     {
-        if (i % 2 != 0)
-        {
-            sum += array[i];
-        }     
+        sum += array[i];
     }
     return sum;
 }

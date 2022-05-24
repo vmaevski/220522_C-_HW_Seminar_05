a8d1313567774c12f54b3ccaae9be15e5d4ddf5d
vmaevski <vmaevski@mail.ru>
2022-05-24 21:36:16 +0700

    Task 3 completed

---
+++ 220522_Task_3/Program.cs
@@ -0,0 +1,47 @@
+﻿// 3. Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] RandomArray(int length, int minValue, int maxValue)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = Convert.ToDouble(new Random().Next(minValue, maxValue + 1)) / Convert.ToDouble(new Random().Next(1, 10));
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{Math.Round(array[i], 2)} ");
+    }
+    Console.WriteLine();
+}
+
+void DiffMaxMin(double[] array)
+{
+    double Diff;
+    double minNum = array[0];
+    double maxNum = array[0];
+    int length = array.Length;
+    for (int i = 1; i < length; i++)
+    {
+        if (array[i] < minNum)
+        {
+            minNum = array[i];
+        }
+        if (array[i] > maxNum)
+        {
+            maxNum = array[i];
+        }
+    }
+    Diff = maxNum - minNum;    
+    Console.WriteLine($"({Math.Round(maxNum, 2)}) - ({Math.Round(minNum, 2)}) = {Math.Round(Diff, 2)}");
+}
+
+double[] array = RandomArray(10, -100, 100);
+PrintArray(array);
+DiffMaxMin(array);

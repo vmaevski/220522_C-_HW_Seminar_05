73d31a9e02e8277b13386f2c144ad52162f0b36c
vmaevski <vmaevski@mail.ru>
2022-05-25 15:43:53 +0700

    Task 4 completed

--- 220522_Task_4/Program.cs
+++ 220522_Task_4/Program.cs
@@ -1,6 +1,27 @@
 ﻿// Найдите произведение пар чисел в одномерном массиве. 
 // Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
 // Результат запишите в новом массиве.
+// *** Непарный элемент массива с нечётным кол-вом элементов будем выводить в конце нового массива.
+
+int[] MultOfPairs(int[] array)
+{
+    int length = array.Length;
+    int newLength = length / 2;
+    if (length % 2 != 0)
+    {
+        newLength++; // для элемента без пары
+    }
+    int[] newArray = new int[newLength];
+    for (int i = 0; i < length / 2; i++)
+    {
+        newArray[i] = array[i] * array[length - 1 - i];
+    }
+    if (length % 2 != 0)
+    {
+        newArray[newLength - 1] = array[newLength - 1]; // добавляем элемент без пары
+    }
+    return newArray;
+}
 
 int[] RandomArray(int length, int minValue, int maxValue)
 {
@@ -22,15 +43,6 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-int[] 1 MultOfPairs(int[] array)
-{
-    int length = array.Length;
-    int newLength = length / 2; // если кол-во элементов массива нечётное, элемент из середины не имеет пары, его игнорируем?
-    int[] newArray = new int[newLength]; 
-    for (int i = 0; i < newLength; i++)
-    {
-        newArray[i] = array[i] * array[length - 1 - i];
-    }
-}
-
-PrintArray(RandomArray(5, 1, 10));
+int[] array = RandomArray(9, 1, 10);
+PrintArray(array);
+PrintArray(MultOfPairs(array));
